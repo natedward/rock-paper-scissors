60c614c51299c70cfd334fe3c6d79635db7e043b
Nathan Ward <natedward@hotmail.com>
2022-03-03 18:19:51 -0500

    added all game outcomes

--- javascript.js
+++ javascript.js
@@ -24,10 +24,39 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection == "rock" && computerSelection == "rock") {
         console.log("Tie");
     }
+    else if (playerSelection == "rock" && computerSelection == "paper") {
+        console.log("comp wins");
+    }
+    else if (playerSelection == "rock" && computerSelection == "scissors") {
+        console.log("player wins");
+    }
+    else if (playerSelection == "paper" && computerSelection == "paper") {
+        console.log("tie");
+    }
+    else if (playerSelection == "paper" && computerSelection == "rock") {
+        console.log("player wins");
+    }
+    else if (playerSelection == "paper" && computerSelection == "scissors") {
+        console.log("comp wins");
+    }
+    else if (playerSelection == "scissors" && computerSelection == "scissors") {
+        console.log("tie");
+    }
+    else if (playerSelection == "scissors" && computerSelection == "rock") {
+        console.log("comp");
+    }
+    else if (playerSelection == "scissors" && computerSelection == "paper") {
+        console.log("player wins");
+    }
+    else if (playerSelection == "scissors" && computerSelection == "scissors") {
+        console.log("tie");
+    }
 }
 
 const computerSelection = computerPlays();
 const playerSelection = playerPlays();
 
+console.log(computerSelection);
+console.log(playerSelection);
 playRound(playerSelection, computerSelection);
 
