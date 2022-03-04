45967b9c780ffe4acab844b6eba870f1b5517309
Nathan Ward <natedward@hotmail.com>
2022-03-03 22:10:58 -0500

    completed general function of program

--- javascript.js
+++ javascript.js
@@ -1,6 +1,10 @@
+let playerScore = 0;
+let computerScore = 0;
+
+
 function computerPlays() {
     let choice;
-    let i = Math.floor(Math.random() * 3);
+    let i = Math.floor(Math.random() * 3); //used to create a random number of either 0, 1, or 2 then using that number to determine computers choice
     if(i == 0){
         choice = "rock";
         return choice;
@@ -15,10 +19,19 @@ function computerPlays() {
     }
 }
 
-function playerPlays() {
-    let choice = prompt("What is your choice?");
-    return choice;
+function winningMessage()
+{
+    if (playerScore === 5)
+    {
+        return "woah, totes cool bro player won";
+    }
+    else if (computerScore === 5)
+    {
+        return "woah, totes cool bro comp won";
+    }
 }
+
+
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == "rock" && computerSelection == "rock") {
@@ -26,37 +39,82 @@ function playRound(playerSelection, computerSelection) {
     }
     else if (playerSelection == "rock" && computerSelection == "paper") {
         console.log("comp wins");
+        return computerScore++;
     }
     else if (playerSelection == "rock" && computerSelection == "scissors") {
         console.log("player wins");
+        return playerScore++;
     }
     else if (playerSelection == "paper" && computerSelection == "paper") {
         console.log("tie");
     }
     else if (playerSelection == "paper" && computerSelection == "rock") {
         console.log("player wins");
+        return playerScore++;
     }
     else if (playerSelection == "paper" && computerSelection == "scissors") {
         console.log("comp wins");
+        return computerScore++;
     }
     else if (playerSelection == "scissors" && computerSelection == "scissors") {
         console.log("tie");
     }
     else if (playerSelection == "scissors" && computerSelection == "rock") {
         console.log("comp");
+        return computerScore++;
     }
     else if (playerSelection == "scissors" && computerSelection == "paper") {
         console.log("player wins");
+        return playerScore++;
     }
     else if (playerSelection == "scissors" && computerSelection == "scissors") {
         console.log("tie");
     }
 }
 
-const computerSelection = computerPlays();
-const playerSelection = playerPlays();
+function game() {
+    while (playerScore <= 5 && computerScore <= 5)
+    {
+        let playerSelection = prompt("What is your choice?"); //prompt the user to input there choice
+        let computerSelection = computerPlays();
+        playRound(playerSelection, computerSelection);
+        console.log(playerScore);
+        console.log(computerScore);
 
-console.log(computerSelection);
-console.log(playerSelection);
-playRound(playerSelection, computerSelection);
+    }
+    console.log(winningMessage());
+
+}
+
+
+
+
+
+
+    /*
+    for (let i = 0; i < 50; i++) 
+    {
+        if (playerScore <= 5 && computerScore <= 5) 
+        {
+            let playerSelection = prompt("What is your choice?"); //prompt the user to input there choice
+            let computerSelection = computerPlays();
+            playRound(playerSelection, computerSelection);
+            console.log(playerScore);
+            console.log(computerScore);
+        }
+        else if (playerScore == 5)
+        {
+            console.log("player wins   !");
+        }
+        else if (computerScore == 5)
+        {
+            console.log("comp wins   !");
+        }
+
+    
+    }*/
+
+
+
+game();
 
